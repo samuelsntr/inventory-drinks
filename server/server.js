7dca9150ea614bae33f64200a32a60ff93b69b16
samuelsntr <dodisamuel20@gmail.com>
2026-02-07 13:50:27 +0800

    fix: remove hardcoded fallback CORS origin

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://inventory-drinks.bountygroup.co.id",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
